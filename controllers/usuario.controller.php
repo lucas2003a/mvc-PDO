b335d8b01a1bcd93747e0a7490f6182a4641bce6
lucas <lucasatuncar1@gmail.com>
2023-04-10 16:00:09 -0500

    validación de credenciales

--- controllers/usuario.controller.php
+++ controllers/usuario.controller.php
@@ -9,7 +9,31 @@ if (isset($_POST['operacion'])){
   if ($_POST['operacion'] == 'login'){
 
     $registro = $usuario->iniciarSesion($_POST['nombreusuario']);
-    echo json_encode($registro);
+
+    //Obheto para contener el resultado
+    $resultado = [
+      "status"  => false,
+      "mensaje" => ""
+    ];
+    
+    if ($registro){
+      //El usuario si existe
+      $claveEncriptada = $registro["claveacceso"];
+
+      //Validar la contraseña
+      if (password_verify($_POST['claveIngresada'], $claveEncriptada)){
+        $resultado["status"] = true;
+        $resultado["mensaje"] = "Bienvenido al sistema";
+      }else{
+        $resultado["mensaje"] = "Error en la contraseña";
+      }
+    }else{
+      //El usuiario si existe
+      $resultado["mensaje"] = "No encontramos el usuario";
+    }
+
+    //Enviamos el objetivo resultado a la vista
+    echo json_encode($resultado);
 
   }
 }
